052b53c4cc8aace545adb1303d417f2034e7a987
n4to4 <n4to4k@gmail.com>
2022-02-06 11:57:58 +0900

    wip

--- todo/cmd/todo/main.go
+++ todo/cmd/todo/main.go
@@ -1,3 +1,35 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/n4to4/cli-apps-in-go-book/todo"
+)
+
+const todoFileName = ".todo.json"
+
+func main() {
+	l := &todo.List{}
+
+	if err := l.Get(todoFileName); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	switch {
+	case len(os.Args) == 1:
+		for _, item := range *l {
+			fmt.Println(item.Task)
+		}
+	default:
+		item := strings.Join(os.Args[1:], " ")
+		l.Add(item)
+
+		if err := l.Save(todoFileName); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+	}
+}
