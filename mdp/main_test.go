d23469c62e3c35150973f6a9b24b0c8c52df9af7
n4to4 <n4to4k@gmail.com>
2022-02-12 11:08:26 +0900

    wip

--- mdp/main_test.go
+++ mdp/main_test.go
@@ -31,3 +31,27 @@ func TestParseContent(t *testing.T) {
 		t.Error("Result content does not match golden file")
 	}
 }
+
+func TestRun(t *testing.T) {
+	if err := run(inputFile); err != nil {
+		t.Fatal(err)
+	}
+
+	result, err := os.ReadFile(resultFile)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected, err := os.ReadFile(goldenFile)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !bytes.Equal(expected, result) {
+		t.Logf("golden:\n%s\n", expected)
+		t.Logf("result:\n%s\n", result)
+		t.Error("Result content does not match golden file")
+	}
+
+	os.Remove(resultFile)
+}
