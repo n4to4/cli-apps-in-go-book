895d3ee8f5a5d70f962919682e5d0d42fdbfa312
n4to4 <n4to4k@gmail.com>
2022-02-12 11:57:29 +0900

    wip

--- mdp/main.go
+++ mdp/main.go
@@ -4,8 +4,8 @@ import (
 	"bytes"
 	"flag"
 	"fmt"
+	"io"
 	"os"
-	"path/filepath"
 
 	"github.com/microcosm-cc/bluemonday"
 	"github.com/russross/blackfriday/v2"
@@ -36,13 +36,13 @@ func main() {
 		os.Exit(1)
 	}
 
-	if err := run(*filename); err != nil {
-		fmt.Fprintln(os.Stderr, err)
+	if err := run(*filename, os.Stderr); err != nil {
+		fmt.Fprintln(os.Stdout, err)
 		os.Exit(1)
 	}
 }
 
-func run(filename string) error {
+func run(filename string, out io.Writer) error {
 	input, err := os.ReadFile(filename)
 	if err != nil {
 		return err
@@ -50,8 +50,15 @@ func run(filename string) error {
 
 	htmlData := parseContent(input)
 
-	outName := fmt.Sprintf("%s.html", filepath.Base(filename))
-	fmt.Println(outName)
+	temp, err := os.CreateTemp("", "mdp*.html")
+	if err != nil {
+		return err
+	}
+	if err := temp.Close(); err != nil {
+		return err
+	}
+	outName := temp.Name()
+	fmt.Fprintln(out, outName)
 
 	return saveHTML(outName, htmlData)
 }
