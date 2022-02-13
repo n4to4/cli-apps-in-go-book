6ce672367e82b1d59806d457e39ffb63d288f004
n4to4 <n4to4k@gmail.com>
2022-02-13 09:19:28 +0900

    result file

--- mdp/main_test.go
+++ mdp/main_test.go
@@ -3,12 +3,12 @@ package main
 import (
 	"bytes"
 	"os"
+	"strings"
 	"testing"
 )
 
 const (
 	inputFile  = "./testdata/test1.md"
-	resultFile = "test1.md.html"
 	goldenFile = "./testdata/test1.md.html"
 )
 
@@ -33,10 +33,13 @@ func TestParseContent(t *testing.T) {
 }
 
 func TestRun(t *testing.T) {
-	if err := run(inputFile); err != nil {
+	var mockStdOut bytes.Buffer
+
+	if err := run(inputFile, &mockStdOut); err != nil {
 		t.Fatal(err)
 	}
 
+	resultFile := strings.TrimSpace(mockStdOut.String())
 	result, err := os.ReadFile(resultFile)
 	if err != nil {
 		t.Fatal(err)
@@ -53,5 +56,5 @@ func TestRun(t *testing.T) {
 		t.Error("Result content does not match golden file")
 	}
 
-	os.Remove(resultFile)
+	// os.Remove(outFile)
 }
