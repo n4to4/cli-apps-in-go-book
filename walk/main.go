cb908c556c63dc418aa984e4c0f54b7476073c9d
n4to4 <n4to4k@gmail.com>
2022-02-17 19:25:48 +0900

    wip

---
+++ walk/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "flag"
+
+type config struct {
+	ext  string
+	size int64
+	list bool
+}
+
+func main() {
+	root := flag.String("root", ".", "Root directory to start")
+	list := flag.Bool("list", false, "List files only")
+	ext := flag.String("ext", "", "File extension to filter out")
+	size := flag.Int64("size", 0, "Minimum file size")
+	flag.Parse()
+
+	c := config{
+		ext:  *ext,
+		size: *size,
+		list: *list,
+	}
+}
