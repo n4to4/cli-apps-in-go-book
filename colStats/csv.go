f938e2e296178c65fc2a60d9a31065404d94f380
n4to4 <n4to4k@gmail.com>
2022-02-24 19:45:46 +0900

    colStats

---
+++ colStats/csv.go
@@ -0,0 +1,17 @@
+package main
+
+type statsFunc func(data []float64) float64
+
+func sum(data []float64) float64 {
+	sum := 0.0
+
+	for _, v := range data {
+		sum += v
+	}
+
+	return sum
+}
+
+func avg(data []float64) float64 {
+	return sum(data) / float64(len(data))
+}
---
+++ colStats/go.mod
@@ -0,0 +1,3 @@
+module github.com/n4to4/cli-apps-in-go-book/colStats
+
+go 1.18
